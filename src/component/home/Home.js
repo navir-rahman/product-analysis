66721309c53943ada3fec2f6c8929b306477ec68
navir-rahman <navirrahman@gmail.com>
2022-04-04 17:01:41 +0600

    home text added

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,10 +1,22 @@
+import './Heme.css'
 import React, { Component } from 'react';
+
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                this is home
+            <div className='homecontainer'>
+                <div className="hometext">
+                    <h1>Portable Emergency Solar Radio</h1>
+                    <h3>2021 NEWEST DESIGN MD-090P: 4000mAh 
+                        replaceable li-ion battery can charge more than one smartphone. 
+                    </h3>
+                    
+                    <button>See review</button>
+                </div>
+                <div className="homeimage">
+                    <img src={'light.webp'} alt="" />
+                </div>
             </div>
         );
     }
